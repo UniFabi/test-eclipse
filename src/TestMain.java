b8ac38028c83343d8167d28971f23c91f6ee0caa
Fabi <schoberfa@gmail.com>
2018-06-03 15:29:21 +0200

    initial commit for eclipse project

---
+++ src/TestMain.java
@@ -0,0 +1,22 @@
+
+public class TestMain {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Runnable r = () -> {
+			try {
+				while(true) {
+					Thread.sleep(1000);
+					System.out.println("Hallo");
+				}
+			}catch(Exception e) {
+				
+			}
+		};
+		
+		Thread t = new Thread(r);
+		t.start();
+
+	}
+
+}
